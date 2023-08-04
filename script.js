4f0fb8513f5c3bb30f8527acbcef3357f8968766
Victoria Verdera <vic.verdera@gmail.com>
2023-08-04 17:10:20 -0300

    Agregando ultimos cambios

--- script.js
+++ script.js
@@ -1,92 +1,102 @@
 
-let salir = true;
+
+let salir = false;
 let respuesta;
 let textoAlimentosRicos = "Alimentos ricos en vitamina ";
+
+
+class Alimento {
+
+    constructor(nombre, calorias, precio) {
+        this.nombre = nombre;
+        this.calorias = calorias;
+        this.precio = precio;
+    }
+
+    caloriasCadaCienGramos() {
+        alert(`100grs de ${this.nombre} tienen ${this.calorias} calorías.`);
+    }
+
+}
+
+let a = ["salmón", "hígado vacuno", "espinaca", "batatas", "zanahoria", "brócoli", "calabaza", "melón", "mango", "manzana"];
+let b = ["cereal integral", "carne", "pescado", "huevo", "legumbre"];
+let c = ["brocoli", "frutilla", "melón", "papa", "tomate"];
+let d = ["pescado graso", "hígado", "yema de huevo", "queso", "hongo"];
+
 let listaAlimentos = inicializoArrayAlimentos();
 
+while (!salir) {
+    respuesta = prompt("Seleccione la opción deseada: [A] Buscar el índice calórico de un alimento; [B] Buscar alimentos por vitamina; [C] Calcular las calorías de una receta. [X] Salir").toUpperCase();
 
-while (salir === true) {
-    respuesta = prompt("Seleccione la opción deseada: [A] Buscar el índice calórico de un alimento; [B] Buscar alimentos por vitamina; [C] Calular las calorías de una receta. [X] Salir")
+    const listaOpciones = ["Selecciona un alimento de la lista: [1] salmón, [2] hígado, [3] huevo, [4]papa, [5] brócoli, [6] tomate, [7] queso, [8] manzana o [9] frutilla", "Selecciona una vitamina: A, B, C, D."];
+
     if (respuesta == "A") {
-        let alimentoCaloria = prompt("Selecciona un alimento de la lista: [1] salmón, [2] hígado, [3] huevo, [4]papa, [5] brócoli, [6] tomate, [7] queso, [8] manzana o [9] frutilla");
-        caloriasDeXAlimento(alimentoCaloria);
+        let alimentoCaloria = prompt(listaOpciones[0]);
+        listaAlimentos[alimentoCaloria - 1].caloriasCadaCienGramos();
         // Buscar el índice calórico de un alimento
     } else if (respuesta == "B") {
         // Buscar alimentos por vitamina
-        let vitamina = prompt("Selecciona una vitamina: A, B, C, D.").toUpperCase();
+        let vitamina = prompt(listaOpciones[1]).toUpperCase();
         queAlimentosTienenXVitamina(vitamina);
     } else if (respuesta == "C") {
         caloriasPorReceta();
-        // Calular las calorías de una receta
+        // Calcular las calorías de una receta
     } else if (respuesta == "X") {
-        salir = false;
+        salir = true;
     } else {
         alert("Respuesta incorrecta, seleccione una opción.");
     }
 }
 
 function queAlimentosTienenXVitamina(vit) {
+    let vitAlimento;
     switch (vit) {
         case 'A':
-            alert(textoAlimentosRicos + "A: Salmón, hígado vacuno, espinacas, batatas, zanahorias, brócoli, calabaza, melón, mango, manzana.");
+            vitAlimento = a.join(", ");
+            alert(`${textoAlimentosRicos} A: ${vitAlimento}`);
             break;
         case 'B':
-            alert(textoAlimentosRicos + "B: Cereales integrales, productos cárnicos, pescados, huevos, legumbres.");
+            vitAlimento = b.join(", ");
+            alert(`${textoAlimentosRicos} B: ${vitAlimento}`);
             break;
         case 'C':
-            alert(textoAlimentosRicos + "C: brócoli, frutilla, melón, papas, tomates.");
+            vitAlimento = c.join(", ");
+            alert(`${textoAlimentosRicos} C: ${vitAlimento}`);
             break;
         case 'D':
-            alert(textoAlimentosRicos + "D: Los pescados grasos, hígado de ganado vacuno, yema de huevo, queso, hongos.");
+            vitAlimento = d.join(", ");
+            alert(`${textoAlimentosRicos} D: ${vitAlimento}`);
             break;
     }
 }
 
 function inicializoArrayAlimentos() {
-    let caloriasPorCienGramos = new Array(2);
-    caloriasPorCienGramos[0] = new Array(2);
-    caloriasPorCienGramos[1] = new Array(2);
-    caloriasPorCienGramos[2] = new Array(2);
-    caloriasPorCienGramos[3] = new Array(2);
-    caloriasPorCienGramos[4] = new Array(2);
-    caloriasPorCienGramos[5] = new Array(2);
-    caloriasPorCienGramos[6] = new Array(2);
-    caloriasPorCienGramos[7] = new Array(2);
-    caloriasPorCienGramos[8] = new Array(2);
+    let calorias = new Array();
 
+    calorias.push(new Alimento("salmon", 208, 100));
 
-    caloriasPorCienGramos[0][0] = ("salmon");
-    caloriasPorCienGramos[0][1] = (208);
+    calorias.push(new Alimento("hígado", 165));
 
-    caloriasPorCienGramos[1][0] = ("hígado");
-    caloriasPorCienGramos[1][1] = (165);
+    calorias.push(new Alimento("huevo", 155));
 
-    caloriasPorCienGramos[2][0] = ("huevo");
-    caloriasPorCienGramos[2][1] = (155);
+    calorias.push(new Alimento("papa", 86));
 
-    caloriasPorCienGramos[3][0] = ("papa");
-    caloriasPorCienGramos[3][1] = (86);
+    calorias.push(new Alimento("brócoli", 34));
 
-    caloriasPorCienGramos[4][0] = ("brócoli");
-    caloriasPorCienGramos[4][1] = (34);
+    calorias.push(new Alimento("tomate", 20));
 
-    caloriasPorCienGramos[5][0] = ("tomate");
-    caloriasPorCienGramos[5][1] = (20);
+    calorias.push(new Alimento("queso", 402));
 
-    caloriasPorCienGramos[6][0] = ("queso");
-    caloriasPorCienGramos[6][1] = (402);
+    calorias.push(new Alimento("manzana", 52));
 
-    caloriasPorCienGramos[7][0] = ("manzana");
-    caloriasPorCienGramos[7][1] = (52);
+    calorias.push(new Alimento("frutilla", 25));
 
-    caloriasPorCienGramos[8][0] = ("frutilla");
-    caloriasPorCienGramos[8][1] = (25);
-
-    return caloriasPorCienGramos;
+    return calorias;
 }
 
 function caloriasDeXAlimento(alimento) {
-    alert("100gr de " + listaAlimentos[alimento - 1][0] + " tienen " + listaAlimentos[alimento - 1][1] + "calorías.");
+    alert("100gr de " + listaAlimentos[alimento - 1].nombre + " tienen " + listaAlimentos[alimento - 1].calorias + "calorías.");
 }
 
 function caloriasPorReceta() {
@@ -95,7 +105,7 @@ function caloriasPorReceta() {
     while (seguir) {
         let respuesta = prompt("Selecciona un alimento de la lista: [1] salmón, [2] hígado, [3] huevo, [4]papa, [5] brócoli, [6] tomate, [7] queso, [8] manzana o [9] frutilla");
         let cantidad = prompt("Ingrese el número de gramos (minimo 100gr).") / 100;
-        sumaCalorias += (listaAlimentos[respuesta - 1][1] * cantidad);
+        sumaCalorias += (listaAlimentos[respuesta - 1].calorias * cantidad);
         if ("N" == prompt("Desea sumar otro alimento? [S] Si, [N] No").toUpperCase()) {
             alert("La suma de calorías para esa receta es: " + sumaCalorias);
             seguir = false;
